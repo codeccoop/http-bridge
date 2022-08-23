46e4872fd47837ebb1c4fc7d2bdeceebcfba5e7f
Pelayo García <pelayo.garcia@coopdevs.org>
2022-08-23 15:01:06 +0200

    feat: map forms to utm.source in options page
    Add the ability to asign each form to a utm.source ERP parameter

--- includes/options-page.php
+++ includes/options-page.php
@@ -1,7 +1,11 @@
 <?php
 
-
-function wpct_odoo_connect_add_admin_menu(  ) { 
+/**
+ * Define menu page.
+ */
+ 
+function wpct_odoo_connect_add_admin_menu()
+{
 
     add_options_page(
         'wpct_odoo_connect',
@@ -10,67 +14,167 @@ function wpct_odoo_connect_add_admin_menu(  ) {
         'wpct_odoo_connect',
         'wpct_odoo_connect_options_page'
     );
-
 }
-add_action( 'admin_menu', 'wpct_odoo_connect_add_admin_menu' );
+add_action('admin_menu', 'wpct_odoo_connect_add_admin_menu');
 
+/**
+ * Define settings.
+ */
+function wpct_odoo_connect_settings_init()
+{
 
-function wpct_odoo_connect_settings_init(  ) { 
-
-    register_setting( 'odooConnectSettingsPage', 'wpct_odoo_connect_settings' );
+    register_setting('odooConnectSettingsPage', 'wpct_odoo_connect_settings');
 
     add_settings_section(
-        'wpct_odoo_connect_odooConnectSettingsPage_section', 
-        __( 'API Key', 'wpct_odoo_connect' ), 
-        'wpct_odoo_connect_settings_section_callback', 
+        'wpct_odoo_connect_odooConnectSettingsPage_section',
+        __('API Key', 'wpct_odoo_connect'),
+        'wpct_odoo_connect_settings_section_callback',
         'odooConnectSettingsPage'
     );
 
-    add_settings_field( 
-        'wpct_odoo_connect_textField_apiKey', 
-        __( 'API Key', 'wpct_odoo_connect' ), 
-        'wpct_odoo_connect_textField_apiKey_render', 
-        'odooConnectSettingsPage', 
-        'wpct_odoo_connect_odooConnectSettingsPage_section' 
+    add_settings_field(
+        'wpct_odoo_connect_textField_apiKey',
+        __('API Key', 'wpct_odoo_connect'),
+        'wpct_odoo_connect_textField_apiKey_render',
+        'odooConnectSettingsPage',
+        'wpct_odoo_connect_odooConnectSettingsPage_section'
     );
 
+    add_settings_section(
+        'wpct_odoo_connect_odooConnectCoordId_section',
+        __('Mapping values', 'wpct_odoo_cood_id'),
+        'wpct_odoo_cood_id_section_callback',
+        'odooConnectSettingsPage'
+    );
 
+    add_settings_field(
+        'wpct_odoo_connect_coord_id',
+        __('Coord Company ID', 'wpct_odoo_cood_id'),
+        'wpct_odoo_connect_coord_id_render',
+        'odooConnectSettingsPage',
+        'wpct_odoo_connect_odooConnectCoordId_section'
+    );
+    register_setting('odooConnectSettingsPage', 'odoo_forms_settings');
+    add_settings_section(
+        'Dropdown_odooConnectSettingsPage_section',
+        __('Map existing forms with the ERP endpoints', 'dropdown'),
+        'Odoo_forms_settings_section_callback',
+        'odooConnectSettingsPage'
+    );
+
+    add_settings_field(
+        'ce_source_creation_ce_proposal',
+        __('Form - New Energy Community (ce_source_creation_ce_proposal)', 'dropdown'),
+        'New_community_select_field_render',
+        'odooConnectSettingsPage',
+        'Dropdown_odooConnectSettingsPage_section'
+    );
+    add_settings_field(
+        'ce_source_future_location_ce_info',
+        __('Form - Interest in zone (ce_source_future_location_ce_info)', 'dropdown'),
+        'Zone_interest_select_field_render',
+        'odooConnectSettingsPage',
+        'Dropdown_odooConnectSettingsPage_section'
+    );
+    add_settings_field(
+        'general_newsletter_form',
+        __('Form - General Newsletter', 'dropdown'),
+        'General_newsletter_select_field_render',
+        'odooConnectSettingsPage',
+        'Dropdown_odooConnectSettingsPage_section'
+    );
 }
-add_action( 'admin_init', 'wpct_odoo_connect_settings_init' );
+
+add_action('admin_init', 'wpct_odoo_connect_settings_init');
 
 
-function wpct_odoo_connect_textField_apiKey_render(  ) {
+/**
+ * Iterate Gravity Forms and extract the form IDs and names
+ */
+function iterate_forms($option_name)
+{
+    $options = get_option('odoo_forms_settings');
+    $selected = 'disabled ';
+    if (!key_exists($option_name, $options) || !$options) {
+        $selected .= 'selected';
+        $options[$option_name] = '';
+    }
+    $result = GFAPI::get_forms();
+    echo "<select name='odoo_forms_settings[" . $option_name . "]'>";
+    echo '<option value="null" ' . $selected . '>Select a form</option>';
 
-    $options = get_option( 'wpct_odoo_connect_settings' );
+    foreach ($result as $key => $form) {
+        echo '<option value="' . $key . '" ' . (($options[$option_name] ? $options[$option_name] : 'null') == $key ? 'selected' : '') . '>' . $form['title'] . '</option>';
+    }
+    echo "</select>";
+}
+
+/**
+ * Render the forms
+ */
+
+function New_community_select_field_render()
+{
+    $option_name = 'ce_source_creation_ce_proposal';
+    iterate_forms($option_name);
+}
+
+function Zone_interest_select_field_render()
+{
+    $option_name = 'ce_source_future_location_ce_info';
+    iterate_forms($option_name);
+}
+
+function General_newsletter_select_field_render()
+{
+    $option_name = 'general_newsletter_form';
+    iterate_forms($option_name);
+}
+
+function wpct_odoo_connect_textField_apiKey_render()
+{
+
+    $options = get_option('wpct_odoo_connect_settings');
     $current_api_key = $options['wpct_odoo_connect_textField_apiKey'] ? $options['wpct_odoo_connect_textField_apiKey'] : '';
-    ?>
-    <input type='text' name='wpct_odoo_connect_settings[wpct_odoo_connect_textField_apiKey]' value="<?php echo $current_api_key; ?>">
-    <?php
+    echo "<input type='text' name='wpct_odoo_connect_settings[wpct_odoo_connect_textField_apiKey]' value='" . $current_api_key . "'>'";
+}
 
+function wpct_odoo_connect_coord_id_render()
+{
+
+    $options = get_option('wpct_odoo_connect_settings');
+    key_exists('wpct_odoo_connect_coord_id', $options) ? $coord_id = $options['wpct_odoo_connect_coord_id'] : $coord_id = '-1';
+    echo "<input type='text' name='wpct_odoo_connect_settings[wpct_odoo_connect_coord_id]' value='" . $coord_id . "'>";
 }
 
 
-function wpct_odoo_connect_settings_section_callback(  ) { 
-
-    echo __( 'Copy here your Odoo API key', 'wpct_odoo_connect' );
-
+/**
+ * Callbacks for the settings sections
+ */
+function wpct_odoo_connect_settings_section_callback()
+{
+    echo __('Copy here your Odoo API key', 'wpct_odoo_connect');
 }
 
+function wpct_odoo_cood_id_section_callback()
+{
+    echo __('Map values from WP to backend settings', 'wpct_odoo_cood_id');
+}
 
-function wpct_odoo_connect_options_page(  ) { 
+function Odoo_forms_settings_section_callback()
+{
+    echo __('Asign the utm.source field to each form', 'dropdown');
+}
 
-    ?>
-    <form action='options.php' method='post'>
-
-        <h2>WPCT Odoo Connect</h2>
-
-        <?php
-        settings_fields( 'odooConnectSettingsPage' );
-        do_settings_sections( 'odooConnectSettingsPage' );
-        submit_button();
-        ?>
-
-    </form>
-    <?php
-
+/**
+ * Paint the settings page
+ */
+function wpct_odoo_connect_options_page()
+{
+    echo "<form action='options.php' method='post'>";
+    echo "<h2>WPCT Odoo Connect</h2>";
+    settings_fields('odooConnectSettingsPage');
+    do_settings_sections('odooConnectSettingsPage');
+    submit_button();
+    echo "</form>";
 }
