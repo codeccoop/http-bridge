0b62c389bf81663b050ee6f53abd33fd7b3498c7
Pelayo García <pelayo.garcia@coopdevs.org>
2022-09-12 15:16:20 +0200

    fix: check for dependencies

--- wpct-odoo-connect.php
+++ wpct-odoo-connect.php
@@ -11,4 +11,16 @@
  *
  * @package         Wpct_Odoo_Connect
  */
+
+ // Check plugin dependencies.
+if (!class_exists('GFAPI')) {
+    add_action('admin_notices', 'wpct_odoo_connect_admin_notices');
+    return;
+}
+
+function wpct_odoo_connect_admin_notices()
+{
+    echo '<div class="error"><p>' . __('WPCT Forms Maps Leads requires Gravity Forms and Gravity Forms Webhook', 'wpct-forms-map-lead') . '</p></div>';
+}
+
 require_once "includes/options-page.php";
