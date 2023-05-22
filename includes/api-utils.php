88e74891e4d2b00a288d0fb5995b26220fb6d4a4
Lucas García <lucas@codeccoop.org>
2023-05-22 12:50:01 +0200

    feat: decouple wpct_oc_set_headers fn interface from gf and use it on wpct_oc_post_odoo

--- includes/api-utils.php
+++ includes/api-utils.php
@@ -1,20 +1,28 @@
 <?php
 
-function wpct_oc_post_odoo($data,$endpoint) {
-	$post_url = wpct_oc_get_odoo_base_url().$endpoint;
+function wpct_oc_post_odoo($data, $endpoint)
+{
+	$url = parse_url($endpoint);
+	if (isset($url['scheme'])) {
+		$post_url = $endpoint;
+	} else {
+		$post_url = wpct_oc_get_odoo_base_url() . $endpoint;
+	}
+
 	$post_data = json_encode($data);
 	$args = array(
-		'headers' => array(
+		'headers' => wpct_oc_set_headers(array(
 			'Content-Type' => 'application/json',
 			'Connection' => 'keep-alive',
-			'accept' => 'application/json',
-			'API-KEY' => wpct_oc_get_api_key()
-		),
+			'Accept' => 'application/json',
+		)),
 		'body' => $post_data
 	);
-	$response = wp_remote_post( $post_url, $args);
-	if ( !is_wp_error( $response ) ) {
+
+	$response = wp_remote_post($post_url, $args);
+	if (!is_wp_error($response)) {
 		return $response;
 	}
+
 	return false;
 }
