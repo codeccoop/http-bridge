0565814a954b3b1f4b87bbf6be727f8a4de05c6e
Lucas García <lucas@codeccoop.org>
2023-08-23 17:14:43 +0200

    feat: replace language code by locale code on Accept-Language header value

--- includes/user-language.php
+++ includes/user-language.php
@@ -2,15 +2,18 @@
 
 function wpct_oc_user_locale()
 {
-    $locale = apply_filters('wpml_current_language', null);
-    if (!$locale) {
+    $language = apply_filters('wpml_post_language_details', null);
+    if ($language) {
+        $locale = $language['locale'];
+    } else {
         $locale = WPCT_OC_DEFAULT_LOCALE;
     }
+
     return $locale;
 }
 
 function wpct_oc_accept_language_header()
 {
     $locale = wpct_oc_user_locale();
-    return $locale . ', es;q=0.5';
+    return $locale . ', ca;q=0.9';
 }
