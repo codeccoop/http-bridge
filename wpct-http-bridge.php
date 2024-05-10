a56a417c68c0bb9314244eed5b94c94e0ecf5206
Lucas García <lucas@codeccoop.org>
2024-05-10 16:52:53 +0200

    feat: version 1.0.5

--- wpct-http-bridge.php
+++ wpct-http-bridge.php
@@ -8,7 +8,7 @@
  * Author URI:      https://www.codeccoop.org
  * Text Domain:     wpct-http
  * Domain Path:     /languages
- * Version:         1.0.4
+ * Version:         1.0.5
  *
  * @package         Wpct_Http
  */
