100ba7a2faa2b27c2f5192fc62bd6d049190305f
Lucas García <lucas@codeccoop.org>
2024-12-06 07:11:59 +0100

    feat: settings sanitization

--- includes/class-settings.php
+++ includes/class-settings.php
@@ -20,6 +20,20 @@ class Settings extends BaseSettings
      */
     protected static $rest_controller_class = '\HTTP_BRIDGE\REST_Settings_Controller';
 
+    public function construct(...$args)
+    {
+        parent::construct(...$args);
+
+        add_filter(
+            'wpct_sanitize_setting',
+            function ($value, $setting) {
+                return $this->sanitize_setting($value, $setting);
+            },
+            10,
+            2
+        );
+    }
+
     /**
      * Register plugin settings.
      */
@@ -70,6 +84,56 @@ class Settings extends BaseSettings
                     ],
                 ],
             ]
+        );
+    }
+
+    private function sanitize_setting($value, $setting)
+    {
+        if ($setting->group() !== $this->group()) {
+            return $value;
+        }
+
+        $name = $setting->name();
+        switch ($name) {
+            case 'general':
+                $value = self::validate_general($value);
+                break;
+        }
+
+        return $value;
+    }
+
+    public static function validate_general($value)
+    {
+        $value['whitelist'] = (bool) $value['whitelist'];
+        $value['backends'] = self::validate_backends($value['backends']);
+        return $value;
+    }
+
+    public static function validate_backends($backends)
+    {
+        $unique_names = [];
+        return array_map(
+            function ($b) {
+                $b['name'] = sanitize_text_field($b['name']);
+                $b['base_url'] = sanitize_text_field($b['base_url']);
+                for ($i = 0; $i < count($b['headers']); $i++) {
+                    $b['headers'][$i]['name'] = sanitize_text_field(
+                        $b['headers'][$i]['name']
+                    );
+                    $b['headers'][$i]['value'] = sanitize_text_field(
+                        $b['headers'][$i]['value']
+                    );
+                }
+                return $b;
+            },
+            array_filter((array) $backends, static function ($backend) use (
+                $unique_names
+            ) {
+                return !in_array($backend['name'], $unique_names) &&
+                    filter_var($backend['base_url'], FILTER_VALIDATE_URL) &&
+                    is_array($backend['headers']);
+            })
         );
     }
 }
