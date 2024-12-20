1d873ce477327f3ea31f120e556186dcea033423
Lucas García <lucas@codeccoop.org>
2024-12-20 11:57:48 +0100

    fix: i18n integration fixes

--- http-bridge.php
+++ http-bridge.php
@@ -8,7 +8,7 @@
  * Author URI:      https://www.codeccoop.org
  * Text Domain:     http-bridge
  * Domain Path:     /languages
- * Version:         1.3.3
+ * Version:         1.3.4
  */
 
 namespace HTTP_BRIDGE;
