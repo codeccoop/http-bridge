523bbf3b735a03dc11021c58ca4d8cd58c9658a7
Lucas García <lucas@codeccoop.org>
2025-10-25 13:33:42 +0200

    feat: project setup

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,20 +1,10 @@
 const path = require("path");
 
-const buildEslintCommand = (filenames) =>
-  `eslint ${filenames.map((f) => path.relative(process.cwd(), f)).join(" ")}`;
-
-const buildPrettierCommand = (filenames) =>
-  `prettier --write --ignore-unknown ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(" ")}`;
-
-const buildPrettierPhpCommand = (filenames) =>
-  `prettier --write --ignore-unknown --config .prettierrc-php ${filenames
+const buildPhpCbfCommand = (filenames) =>
+  `vendor/bin/phpcbf -n ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(" ")}`;
 
 module.exports = {
-  // "src/**/*.{js,jsx,ts,tsx}": [buildEslintCommand],
-  "*.{json,js,ts,jsx,tsx,html,css}": [buildPrettierCommand],
-  "*.php": [buildPrettierPhpCommand],
+  "*.php": [buildPhpCbfCommand],
 };
